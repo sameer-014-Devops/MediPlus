232b01f5ea53d40ceb330190faf5d4c6b7434ff1
Sameer A <sksameer4970@gmail.com>
2025-02-12 10:29:00 +0530

    Add files via upload

---
+++ src/test/java/com/mediplus/healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediplus.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
