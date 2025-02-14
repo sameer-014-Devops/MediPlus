c883855847b15653dd8557afd82eec923910c4ea
Sameer A <sksameer4970@gmail.com>
2025-02-14 18:03:30 +0530

    Update Mediplus_Testing_Pipeline.groovy

--- Jenkins/Mediplus_Testing_Pipeline.groovy
+++ Jenkins/Mediplus_Testing_Pipeline.groovy
@@ -261,7 +261,7 @@ pipeline {
             steps{
                 sh """mkdir -p $Prod_Workspace"""
                 sh """cp -R Dockerfile *.yaml target $Prod_Workspace"""
-                echo '**********Copied the Dockerfile, mediplusdeploy.yaml, and target files to the Medicure Production workspace**********'
+                echo '**********Copied the Dockerfile, mediplusdeploy.yaml, and target files to the Mediplus Production workspace**********'
                 cleanWs()
                 deleteDir()
             }
